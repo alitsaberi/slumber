78f297fc0122901b142184e8a43fd9dc376167b9
Ali Saberi <ali.saberi@radboudumc.nl>
2025-01-03 17:48:48 +0100

    Complete zmax unit logic

--- slumber/dag/units/zmax.py
+++ slumber/dag/units/zmax.py
@@ -1,10 +1,13 @@
 import asyncio
+import time
+from collections import deque
 from collections.abc import AsyncGenerator
 from functools import cached_property
 from typing import Annotated
 
 import ezmsg.core as ez
 import numpy as np
+from ezmsg.util.rate import Rate
 from loguru import logger
 from pydantic import AfterValidator, ConfigDict
 
@@ -18,8 +21,8 @@ from slumber.utils.helpers import enum_by_name_validator
 class Settings(PydanticSettings):
     zmax: Annotated[ZMax, AfterValidator(is_connected)]
     data_types: list[Annotated[DataType, enum_by_name_validator(DataType)]]
-    buffer_size: int = 10 * ZMAX_SAMPLE_RATE  # 10 seconds
-    retry_delay: int = 5
+    buffer_duration: int = 10  # seconds
+    retry_delay: int = 5  # seconds
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -27,9 +30,14 @@ class Settings(PydanticSettings):
     def channel_names(self) -> list[str]:
         return [data_type.name for data_type in self.data_types]
 
+    @property
+    def buffer_size(self) -> int:
+        return self.buffer_duration * ZMAX_SAMPLE_RATE
+
 
 class State(ez.State):
-    buffer: list[np.ndarray]
+    buffer: deque
+    publish_rate: Rate
 
 
 class ZMaxDataReceiver(ez.Unit):
@@ -38,34 +46,80 @@ class ZMaxDataReceiver(ez.Unit):
     OUTPUT_DATA = ez.OutputStream(Data)
 
     def initialize(self) -> None:
-        self.STATE.buffer = []
+        self.STATE.buffer = deque(maxlen=self.SETTINGS.buffer_size * 2)
+        self.STATE.publish_rate = Rate(1 / self.SETTINGS.buffer_duration)
 
-    @ez.publisher(OUTPUT_DATA)
-    async def collect(self) -> AsyncGenerator:
+    @ez.task
+    async def receive(self) -> AsyncGenerator:
         while True:
             try:
                 sample = self.SETTINGS.zmax.read(self.SETTINGS.data_types)
+                timestamp = time.time()
+                self.STATE.buffer.append((timestamp, sample))
             except TimeoutError:
                 logger.warning(
                     "Timed out while waiting for data from ZMax."
                     f"Retrying in {self.SETTINGS.retry_delay} seconds..."
                 )
                 await asyncio.sleep(self.SETTINGS.retry_delay)
-                continue
-            except ConnectionError:
-                logger.error("Connection to ZMax lost. Reconnecting...")
+            except ConnectionError as e:
+                logger.error(f"Error connecting to ZMax: {e}. Retrying to connect...")
                 self.SETTINGS.zmax.connect()
-                continue
 
-            self.STATE.buffer(sample)
+    @ez.publisher(OUTPUT_DATA)
+    async def publish(self):
+        while True:
+            current_time = time.time()
+            expected_buffer_end_time = (
+                self.STATE.publish_rate.last_time + self.SETTINGS.buffer_duration
+            )
 
-            if len(self.STATE.buffer) >= self.SETTINGS.buffer_size:
-                yield (
-                    self.OUTPUT_DATA,
-                    Data(
-                        np.array(self.STATE.buffer),
-                        sample_rate=ZMAX_SAMPLE_RATE,
-                        channel_names=self.SETTINGS.channel_names,
-                    ),
+            if expected_buffer_end_time < current_time:
+                logger.warning(
+                    f"Buffer is too old. Expected end time: {expected_buffer_end_time},"
+                    f" current time: {current_time}"
                 )
-                self.STATE.buffer = []
+
+            await self.STATE.publish_rate.sleep()
+            yield (self.OUTPUT_DATA, self._process_buffer())
+
+    def _process_buffer(self) -> Data:
+        current_time = time.time()
+        current_array = np.array(self.STATE.buffer)
+
+        if len(current_array) == 0:
+            logger.warning("Buffer is empty. Returning empty data.")
+            return Data(
+                np.zeros((self.SETTINGS.buffer_size, len(self.SETTINGS.channel_names))),
+                sample_rate=ZMAX_SAMPLE_RATE,
+                channel_names=self.SETTINGS.channel_names,
+            )
+
+        timestamps = current_array[:, 0]
+        samples = current_array[:, 1]
+
+        # Get samples only from past duration
+        duration_start = current_time - self.SETTINGS.buffer_duration
+        mask = timestamps >= duration_start
+        timestamps = timestamps[mask]
+        samples = samples[mask]
+
+        if np.any(~mask):
+            logger.warning(
+                f"Found {np.sum(~mask)} samples older than"
+                f" {self.SETTINGS.buffer_duration} seconds."
+            )
+
+        regular_timestamps = np.linspace(
+            duration_start,
+            current_time,
+            self.SETTINGS.buffer_size,
+        )
+
+        interpolated_samples = np.interp(regular_timestamps, timestamps, samples)
+
+        return Data(
+            interpolated_samples,
+            sample_rate=ZMAX_SAMPLE_RATE,
+            channel_names=self.SETTINGS.channel_names,
+        )
