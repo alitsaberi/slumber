902bde7e684d97d376a3311f8387a2523a328dac
Ali Saberi <ali.saberi96@gmail.com>
2025-02-12 12:11:38 +0100

    Update ComponentConfig and CollectionConfig for better validation and not applying the settings when validating

--- slumber/models/dag.py
+++ slumber/models/dag.py
@@ -1,103 +1,146 @@
-from typing import Annotated, Any
+import re
+from functools import cached_property
+from typing import Any, TypeAlias
 
 import ezmsg.core as ez
 import ezmsg.util as ezutil
 from loguru import logger
-from pydantic import BaseModel, BeforeValidator, ConfigDict, Field, model_validator
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    field_validator,
+    model_validator,
+)
 
 from slumber.dag import units
 from slumber.utils.helpers import create_class_by_name_resolver
 
+StreamConnection: TypeAlias = tuple[str, str]
+
 
 class ComponentConfig(BaseModel):
-    unit: Annotated[
-        type[ez.Unit],
-        BeforeValidator(create_class_by_name_resolver([units, ezutil], ez.Unit)),
-    ]
-    settings: dict[str, Any] = Field(default_factory=dict)
-    # TODO: validate based on unit.SETTINGS
-
-    model_config = ConfigDict(strict=False, arbitrary_types_allowed=True)
-
-    def configure(self) -> ez.Unit:
-        if hasattr(self.unit.SETTINGS, "model_validate"):
-            settings = self.unit.SETTINGS.model_validate(self.settings)
-        else:
-            logger.warning(
-                f"Settings for {self.unit.__name__} are not validated. "
-                "This may cause unexpected behavior."
-            )
-            settings = self.unit.SETTINGS(**self.settings)
-        logger.info(f"Configured {self.unit.__name__} with settings: {settings}")
-        return self.unit(settings)
-
-
-class CollectionConfig(BaseModel):
-    components: dict[str, ez.Unit] = Field(min_length=1)
-    connections: tuple[tuple[ez.OutputStream, ez.InputStream], ...] = Field(
-        default_factory=tuple
-    )
-    process_components: tuple[ez.Unit, ...] = Field(default_factory=tuple)
-    name: str | None = Field(None, serialization_alias="root_name")
+    name: str = Field(min_length=1, upper=True)
+    unit: type[ez.Unit]
+    settings: ez.Settings | None = None
 
     model_config = ConfigDict(strict=False, arbitrary_types_allowed=True)
 
     @model_validator(mode="before")
     @classmethod
-    def resolve_connections(cls, values: dict[str, Any]) -> dict[str, Any]:
-        connections = values.get("connections", tuple())
-        components = cls._resolve_components(values.get("components", {}))
+    def validate_settings(cls, data: dict[str, Any]) -> dict[str, Any]:
+        if "unit" not in data:
+            return data
 
-        if (process_components := values.get("process_components")) is not None:
-            values["process_components"] = tuple(
-                components[component_name] for component_name in process_components
-            )
+        unit = create_class_by_name_resolver([units, ezutil], ez.Unit)(data["unit"])
 
-        values["connections"] = tuple(
-            (
-                cls._resolve_stream(connection[0], components),
-                cls._resolve_stream(connection[1], components),
-            )
-            for connection in connections
-        )
-        values["components"] = components
+        if "settings" in data:
+            settings = data["settings"]
 
-        return values
-
-    @staticmethod
-    def _resolve_components(
-        components: dict[str, dict[str, Any]],
-    ) -> dict[str, ez.Unit]:
-        return {
-            name: ComponentConfig.model_validate(component_config).configure()
-            for name, component_config in components.items()
-        }
-
-    @staticmethod
-    def _resolve_stream(
-        stream: str, components: dict[str, ez.Unit]
-    ) -> ez.InputStream | ez.OutputStream:
-        if isinstance(stream, str):
-            try:
-                component_name, stream_name = stream.split(".")
-            except ValueError as e:
+            if not hasattr(unit, "SETTINGS"):
                 raise ValueError(
-                    f"Invalid stream name: {stream}."
-                    f" Expected format: <unit_name>.<stream_name>"
-                ) from e
-
-            if component_name not in components:
-                raise ValueError(
-                    f"Invalid component name for a stream: {component_name}. "
-                    f"Expected one of: {list(components.keys())}"
+                    f"Unit {unit.__name__} does not have a SETTINGS attribute."
                 )
 
-            try:
-                stream = getattr(components[component_name], stream_name)
-            except AttributeError as e:
-                raise ValueError(
-                    f"Invalid stream name: {stream_name} for component:"
-                    f" {component_name}."
-                ) from e
+            if hasattr(unit.SETTINGS, "model_validate"):
+                settings = unit.SETTINGS.model_validate(settings)
+            elif isinstance(settings, dict):
+                logger.warning(
+                    f"Settings for {unit.__name__} are not validated. "
+                    "This may cause unexpected behavior."
+                )
+                settings = unit.SETTINGS(**data["settings"])
 
-        return stream
+            data["settings"] = settings
+
+        data["unit"] = unit
+
+        return data
+
+    @field_validator("identifier", mode="after")
+    @classmethod
+    def check_uppercase_underscore(cls, value: str) -> str:
+        if not re.fullmatch(r"[A-Z]+(?:_[A-Z]+)*", value):
+            raise ValueError(
+                "Must be uppercase with words separated by underscores"
+                " (e.g., 'HELLO_WORLD')."
+            )
+        return value
+
+    def configure(self) -> ez.Unit:
+        logger.info(f"Configured {self.unit.__name__} with settings: {self.settings}")
+        return self.unit(self.settings)
+
+
+class CollectionConfig(BaseModel):
+    components: list[ComponentConfig] = Field(min_length=1)
+    connections: list[StreamConnection] = Field(default_factory=list)
+    process_components: list[str] = Field(default_factory=list)
+    name: str | None = Field(None, serialization_alias="root_name")
+
+    model_config = ConfigDict(strict=False, arbitrary_types_allowed=True)
+
+    @cached_property
+    def components_mapping(self) -> dict[str, ComponentConfig]:
+        return {component.name: component for component in self.components}
+
+    def has_component(self, name: str) -> bool:
+        return name in self.components_mapping
+
+    @model_validator(mode="after")
+    def validate_unique_components(self) -> "CollectionConfig":
+        names = [component.name for component in self.components]
+        if len(names) != len(set(names)):
+            raise ValueError("Component names must be unique")
+        return self
+
+    @model_validator(mode="after")
+    def validate_connections(self) -> "CollectionConfig":
+        for output_stream, input_stream in self.connections:
+            self._validate_stream(output_stream, ez.OutputStream)
+            self._validate_stream(input_stream, ez.InputStream)
+
+        return self
+
+    def _validate_stream(
+        self, stream: str, stream_type: type[ez.InputStream] | type[ez.OutputStream]
+    ) -> None:
+        try:
+            component_name, stream_name = stream.split(".")
+        except ValueError as e:
+            raise ValueError(
+                f"Invalid stream name: {stream}."
+                f" Expected format: <unit_name>.<stream_name>"
+            ) from e
+
+        if not self.has_component(component_name):
+            available = ", ".join(self.components_mapping.keys())
+            raise ValueError(
+                f"Component '{component_name}' not found."
+                f" Available components: {available}"
+            )
+
+        component = self.components_mapping[component_name]
+        try:
+            stream_obj = getattr(component.unit, stream_name)
+        except AttributeError as e:
+            raise ValueError(
+                f"Stream '{stream_name}' not found in component '{component_name}'. "
+            ) from e
+
+        if not isinstance(stream_obj, stream_type):
+            raise ValueError(
+                f"Stream '{stream}' has wrong type {type(stream_obj).__name__}. "
+                f"Expected {stream_type.__name__}"
+            )
+
+    @model_validator(mode="after")
+    def validate_process_components(self) -> "CollectionConfig":
+        for component in self.process_components:
+            if component not in self.components_mapping:
+                raise ValueError(
+                    f"Invalid component name for a process component: {component}. "
+                    f"Expected one of: {list(self.components_mapping.keys())}"
+                )
+
+        return self
