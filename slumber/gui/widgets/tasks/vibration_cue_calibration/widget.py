a5df5210459a75fd65790211201816c609443ba9
Ali Saberi <ali.saberi96@gmail.com>
2025-02-12 15:25:19 +0100

    Add vibration calibration page

---
+++ slumber/gui/widgets/tasks/vibration_cue_calibration/widget.py
@@ -0,0 +1,172 @@
+from enum import Enum, auto
+
+from loguru import logger
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QDialog, QMessageBox, QPushButton, QWidget
+
+from slumber.dag.units.home_lucid_dreaming.cueing import CueIntensityConfig
+from slumber.dag.units.zmax import ZMaxStimulationSignal
+from slumber.gui.widgets.tasks.base import TaskPage
+from slumber.sources.zmax import LEDColor, ZMax
+
+from .widget_ui import Ui_VibrationCueCalibrationPage
+
+
+class Status(Enum):
+    TO_START_CUEING = (
+        "Ready to start cueing. Click `Present Cue` to start.",
+        "color: #2196F3; font-weight: bold; padding: 10px;",
+    )
+    CUEING = (
+        "Delivering cue in...",
+        "color: #FFC107; font-weight: bold; padding: 10px;",
+    )
+    CUED = (
+        "Cue delivered!",
+        "color: #FFC107; font-weight: bold; padding: 10px;",
+    )
+    FAILURE = (
+        "❌ Failed to deliver light cue. Please try again.",
+        "color: #F44336; font-weight: bold; padding: 10px;",
+    )
+    SUCCESS = (
+        "✅ Calibration successful!",
+        "color: #4CAF50; font-weight: bold; padding: 10px;",
+    )
+
+
+class ButtonStyle(Enum):
+    DISABLED = """
+        background-color: #B0BEC5;
+        color: white;
+        font-weight: bold;
+        padding: 10px;
+        border-radius: 5px;
+        margin-bottom: 25px;
+    """
+    ENABLED = """
+        background: qlineargradient(x1:0, y1:0, x2:1, 
+            y2:1, stop:0 #4CAF50, stop:1 #2E7D32);
+        color: white;
+        font-weight: bold;
+        padding: 10px;
+        border-radius: 5px;
+        margin-bottom: 25px;
+    """
+
+
+PERCEPTION_QUESTION_BOX_TITLE = "Perception"
+PERCEPTION_QUESTION = "Did you perceive the vibration cue?"
+
+
+def set_button_enabled(button: QPushButton, enabled: bool) -> None:
+    button.setEnabled(enabled)
+    style = ButtonStyle.ENABLED if enabled else ButtonStyle.DISABLED
+    button.setStyleSheet(style.value)
+
+
+class VibrationCueCalibrationPage(TaskPage, Ui_VibrationCueCalibrationPage):
+    def __init__(
+        self,
+        index: int,
+        title: str,
+        on_duration: int,
+        off_duration: int,
+        repetitions: int,
+        countdown_seconds: int,
+        parent: QWidget | None = None,
+    ) -> None:
+        super().__init__(parent)
+        self.setupUi(self)
+        self.index = index
+        self.title.setText(title)
+
+        self.stimulation_signal = ZMaxStimulationSignal(
+            vibration=True,
+            led_color=LEDColor.OFF,
+            repetitions=repetitions,
+            on_duration=on_duration,
+            off_duration=off_duration,
+        )
+
+        self.countdown_seconds = countdown_seconds
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self._update_countdown)
+        self.zmax = ZMax()
+
+        self._init_status()
+        self._connect_signals()
+
+    def _init_status(self) -> None:
+        logger.info("Initializing cueing state")
+        self._update_status(Status.TO_START_CUEING)
+        set_button_enabled(self.cue_button, True)
+
+    def _connect_signals(self) -> None:
+        self.cue_button.clicked.connect(self._on_cue_button_click)
+
+    def _update_status(self, status: Enum) -> None:
+        logger.info(f"Updating status to {status}")
+        self.status_label.setText(status.value[0])
+        self.status_label.setStyleSheet(status.value[1])
+
+    def _on_cue_button_click(self) -> None:
+        logger.info("Cue button clicked")
+        self._update_status(Status.CUEING)
+        set_button_enabled(self.cue_button, False)
+        self.remaining_seconds = self.countdown_seconds
+        self._update_countdown()
+        self.timer.start()
+
+    def _update_countdown(self) -> None:
+        self.timer_display.display(self.remaining_seconds)
+        if self.remaining_seconds > 0:
+            self.remaining_seconds -= 1
+        else:
+            self.timer.stop()
+            self._deliver_cue()
+
+    def _deliver_cue(self) -> None:
+        logger.info(f"Delivering cue: {self.stimulation_signal}")
+        try:
+            self.zmax.connect()
+            self.zmax.stimulate(**self.stimulation_signal.model_dump())
+            logger.info("Cue delivered", stimulation_signal=self.stimulation_signal)
+            self._update_status(Status.CUED)
+            self._show_perception_question()
+        except Exception as e:
+            logger.error(f"Error delivering light cue: {e}")
+            self._update_status(Status.FAILURE)
+
+    def _show_perception_question(self):
+        logger.info("Showing perception question")
+        reply = QMessageBox.question(
+            self,
+            PERCEPTION_QUESTION_BOX_TITLE,
+            PERCEPTION_QUESTION,
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No,
+        )
+        logger.info(
+            f"Perception question reply: {'yes' if reply == QMessageBox.Yes else 'no'}"
+        )
+
+        if reply == QMessageBox.Yes:
+            self._finish_calibration()
+            return
+
+        self._init_status()
+
+    def _finish_calibration(self) -> None:
+        logger.info("Finishing calibration")
+        self._update_status(Status.SUCCESS)
+        self.zmax.disconnect()
+        self.done()
+
+    def _init_info_dialog(self) -> QDialog:
+        from .info_ui import Ui_InfoDialog
+
+        dialog = QDialog(self)
+        Ui_InfoDialog().setupUi(dialog)
+        return dialog
---
+++ slumber/gui/widgets/tasks/vibration_cue_calibration/widget_ui.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLCDNumber, QLabel,
+    QLayout, QPushButton, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
+
+class Ui_VibrationCueCalibrationPage(object):
+    def setupUi(self, VibrationCueCalibrationPage):
+        if not VibrationCueCalibrationPage.objectName():
+            VibrationCueCalibrationPage.setObjectName(u"VibrationCueCalibrationPage")
+        VibrationCueCalibrationPage.resize(800, 600)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(VibrationCueCalibrationPage.sizePolicy().hasHeightForWidth())
+        VibrationCueCalibrationPage.setSizePolicy(sizePolicy)
+        VibrationCueCalibrationPage.setMaximumSize(QSize(3840, 2160))
+        self.verticalLayout = QVBoxLayout(VibrationCueCalibrationPage)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.header = QHBoxLayout()
+        self.header.setObjectName(u"header")
+        self.header.setSizeConstraint(QLayout.SizeConstraint.SetMaximumSize)
+        self.title = QLabel(VibrationCueCalibrationPage)
+        self.title.setObjectName(u"title")
+        self.title.setMaximumSize(QSize(16777215, 100))
+        font = QFont()
+        font.setFamilies([u"Arial"])
+        font.setPointSize(36)
+        self.title.setFont(font)
+
+        self.header.addWidget(self.title)
+
+        self.horizontal_spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.header.addItem(self.horizontal_spacer)
+
+        self.info_button = QPushButton(VibrationCueCalibrationPage)
+        self.info_button.setObjectName(u"info_button")
+        self.info_button.setMinimumSize(QSize(50, 50))
+        self.info_button.setMaximumSize(QSize(50, 50))
+        self.info_button.setStyleSheet(u"QPushButton {\n"
+"                                    color: rgb(255, 255, 255);\n"
+"                                    background-color: rgb(1, 92, 187);\n"
+"                                    border: 1px;\n"
+"                                    border-radius: 10px;\n"
+"                                    }\n"
+"\n"
+"                                    QPushButton:hover {\n"
+"                                    color: rgb(220,221,229);\n"
+"                                    background-color: rgb(37, 150, 190);\n"
+"                                    }\n"
+"\n"
+"                                    QPushButton:pressed {\n"
+"                                    color: rgb(220,221,229);\n"
+"                                    background-color: rgb(37, 150, 190);\n"
+"                                    }")
+        icon = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogInformation))
+        self.info_button.setIcon(icon)
+        self.info_button.setIconSize(QSize(40, 40))
+
+        self.header.addWidget(self.info_button)
+
+
+        self.verticalLayout.addLayout(self.header)
+
+        self.body = QVBoxLayout()
+        self.body.setObjectName(u"body")
+        self.body.setSizeConstraint(QLayout.SizeConstraint.SetMaximumSize)
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.body.addItem(self.verticalSpacer)
+
+        self.cue_button = QPushButton(VibrationCueCalibrationPage)
+        self.cue_button.setObjectName(u"cue_button")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.cue_button.sizePolicy().hasHeightForWidth())
+        self.cue_button.setSizePolicy(sizePolicy1)
+        self.cue_button.setStyleSheet(u"background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #2196F3, stop:1 #1976D2);\n"
+"color: white;\n"
+"font-weight: bold;\n"
+"padding: 10px;\n"
+"border-radius: 5px;")
+
+        self.body.addWidget(self.cue_button, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+
+        self.status_label = QLabel(VibrationCueCalibrationPage)
+        self.status_label.setObjectName(u"status_label")
+        sizePolicy1.setHeightForWidth(self.status_label.sizePolicy().hasHeightForWidth())
+        self.status_label.setSizePolicy(sizePolicy1)
+        font1 = QFont()
+        font1.setFamilies([u"Arial"])
+        font1.setPointSize(14)
+        self.status_label.setFont(font1)
+        self.status_label.setStyleSheet(u"background-color: rgba(255, 255, 255, 0.1);\n"
+"            color: white;\n"
+"            padding: 10px;\n"
+"            border-radius: 10px;")
+
+        self.body.addWidget(self.status_label, 0, Qt.AlignmentFlag.AlignHCenter)
+
+        self.timer_display = QLCDNumber(VibrationCueCalibrationPage)
+        self.timer_display.setObjectName(u"timer_display")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.timer_display.sizePolicy().hasHeightForWidth())
+        self.timer_display.setSizePolicy(sizePolicy2)
+        self.timer_display.setMinimumSize(QSize(50, 50))
+        self.timer_display.setBaseSize(QSize(50, 50))
+        self.timer_display.setDigitCount(2)
+
+        self.body.addWidget(self.timer_display, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.body.addItem(self.verticalSpacer_2)
+
+
+        self.verticalLayout.addLayout(self.body)
+
+
+        self.retranslateUi(VibrationCueCalibrationPage)
+
+        QMetaObject.connectSlotsByName(VibrationCueCalibrationPage)
+    # setupUi
+
+    def retranslateUi(self, VibrationCueCalibrationPage):
+        VibrationCueCalibrationPage.setWindowTitle(QCoreApplication.translate("VibrationCueCalibrationPage", u"{{ header}}", None))
+        self.title.setText(QCoreApplication.translate("VibrationCueCalibrationPage", u"{{ header }}", None))
+        self.info_button.setText("")
+        self.cue_button.setText(QCoreApplication.translate("VibrationCueCalibrationPage", u"Present Cue", None))
+        self.status_label.setText("")
+    # retranslateUi
+
